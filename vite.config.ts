ffa6e0e9c1fc2fb6f122d262e6754a40efead995
Yusuke Kawamoto <novogrammer@gmail.com>
2026-01-02 18:50:43 +0900

    複数html書き出しのためにvite.config.tsを追加

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about.html"),
+      },
+    },
+  },
+});
